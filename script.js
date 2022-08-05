5d0bf055d8f0c7a14bd9ecd5a61076b369d3313d
shubham <pshubham1886@gmail.com>
2022-08-05 18:31:21 +0530

    generateItems func done

--- script.js
+++ script.js
@@ -11,7 +11,47 @@ function getItems(){
                 price: doc.data().price
             })
         });
-        console.log(items)
+        // console.log(items)
+        generateItems(items)
+    })
+}
+
+function addToCart(item){
+    console.log('add to cart clicked', item)
+}
+
+function generateItems(items) {
+    let itemHTML = ""
+    items.forEach((item) => {
+        let doc = document.createElement("div");
+        doc.classList.add("main-product", "mr-5");
+        doc.innerHTML = `
+            <div class="product-image w-48 h-52 bg-white rounded-lg p-4">
+                <img class="w-full h-full object-contain" src="${item.image}">
+            </div>
+            <div class="product-name text-gray-700 font-bold mt-2 text-sm">
+                ${item.name}
+            </div>
+            <div class="product-make text-green-700 font-bold">
+                ${item.make}
+            </div>
+            <div class="product-rating text-yellow-300 font-bold my-1">
+            ⭐⭐⭐⭐⭐ ${item.rating}
+            </div>
+            <div class="product-price font-bold text-gray-700 text-lg">
+                ${numeral(item.price).format('$0,0.00')}
+            </div>
+        `
+
+        let addToCartEl = document.createElement("div");
+        addToCartEl.classList.add("hover:bg-yellow-600", "cursor-pointer", "product-add", "h-8", "w-28", "rounded", "bg-yellow-500", "text-white", "text-md", "flex", "justify-center", "items-center");
+        addToCartEl.innerText = "Add to cart";
+        addToCartEl.addEventListener("click", function(){
+            addToCart(item)
+        })
+        doc.appendChild(addToCartEl);
+        document.querySelector(".main-section-products").appendChild(doc);
+
     })
 }
 
