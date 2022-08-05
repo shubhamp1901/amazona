318ec0ed0cc898f00a3e4c5eaa588d7e86b88381
shubham <pshubham1886@gmail.com>
2022-08-05 17:44:00 +0530

    get items function done

---
+++ script.js
@@ -0,0 +1,18 @@
+function getItems(){
+    db.collection("items").get().then((querySnapshot) => {
+        let items = [];
+        querySnapshot.forEach((doc) => {
+            items.push({
+                id: doc.id,
+                image: doc.data().image,
+                name: doc.data().name,
+                make: doc.data().make,
+                rating: doc.data().rating,
+                price: doc.data().price
+            })
+        });
+        console.log(items)
+    })
+}
+
+getItems()
